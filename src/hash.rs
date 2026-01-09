387a255dc1b1aa2086ce45c8f37ca561b9cd4054
FjZ345 <fjz345@hotmail.com>
2026-01-09 01:00:01 +0100

    blake3 had feature flag for mmap

--- src/hash.rs
+++ src/hash.rs
@@ -1,20 +1,11 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-};
-
-use memmap2::MmapOptions;
+use std::io::{self};
 
 pub fn hash_file(path: &str) -> io::Result<String> {
-    let file = File::open(path)?;
-
     // Safety: Memory mapping is unsafe because the file could be
     // truncated by another process while we are reading it.
     // For a file hasher, this is a standard risk to accept.
-    let mmap = unsafe { MmapOptions::new().map(&file)? };
-
     let mut hasher = blake3::Hasher::new();
-    hasher.update_rayon(&mmap);
+    hasher.update_mmap_rayon(&path)?;
 
     Ok(hasher.finalize().to_hex().to_string())
 }
