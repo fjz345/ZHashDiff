3bd4a0e405044f1db35bf4622cc6fb24169dd65b
FjZ345 <fjz345@hotmail.com>
2026-05-13 00:32:24 +0200

    Started on quick_diff feature

--- crates/zdiff-gui/README.md
+++ crates/zdiff-gui/README.md
@@ -6,14 +6,24 @@ GUI for zdiff crate
 
 # Featuers
 * Myers diffing
+* Keybindings
 * Powerful diffing view
-* Line Pivot
+    - Next conflict
+    - Search for text
+    - Goto line
+    - Line Pivot
+    - Lexer modes
+    - Diff Options
+        + Ignore whitespace
+        + Highlight rows that differ
+        + Inline ghost tokens
+        + Syntax highlight (only hardcoded keywords for now)
 
 ## Todo
 * Fix memory consumption for big diffs ('binary search' and/or maybe start new diff when at the diagonal?)
 
 * P4 integration for paths
-* Quick diff tool (+keyboard shortcut)
+* Quick diff tool
 
 * user defined comparison per file format
 * better horizontal scrolling
---
+++ crates/zdiff-gui/src/gui/quick_diff.rs
@@ -0,0 +1,91 @@
+use std::ffi::{OsStr, OsString};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+use tempfile::{NamedTempFile, TempPath};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum UniversalPath {
+    /// Represented as //stream/path/file.txt
+    Depot(String),
+    /// Represented as C:\User\File.txt or /home/user/file.txt
+    Local(PathBuf),
+}
+
+impl UniversalPath {
+    pub fn new<S: AsRef<OsStr>>(s: S) -> Self {
+        let os_str = s.as_ref();
+        let cow = os_str.to_string_lossy();
+
+        if cow.starts_with("//") {
+            Self::Depot(cow.into_owned())
+        } else {
+            Self::Local(PathBuf::from(os_str))
+        }
+    }
+
+    pub fn as_local_path(&self) -> Option<&Path> {
+        match self {
+            Self::Local(p) => Some(p.as_path()),
+            Self::Depot(_) => None,
+        }
+    }
+
+    pub fn to_p4_string(&self) -> String {
+        match self {
+            Self::Depot(s) => s.clone(),
+            Self::Local(p) => p.to_string_lossy().replace('\\', "/"),
+        }
+    }
+
+    pub fn is_depot(&self) -> bool {
+        matches!(self, Self::Depot(_))
+    }
+}
+
+impl AsRef<OsStr> for UniversalPath {
+    fn as_ref(&self) -> &OsStr {
+        match self {
+            Self::Depot(s) => OsStr::new(s),
+            Self::Local(p) => p.as_os_str(),
+        }
+    }
+}
+
+pub fn quick_diff_process_paths(
+    path_source: &UniversalPath,
+    path_target: &UniversalPath,
+) -> (PathBuf, PathBuf) {
+    let handle_path = |u_path: &UniversalPath| -> PathBuf {
+        match u_path {
+            UniversalPath::Local(p) => p.to_path_buf(),
+            UniversalPath::Depot(_depot_str) => {
+                let mut tmp = NamedTempFile::new().expect("Failed to create tmp file");
+
+                // Here you would actually call 'p4 print' or similar
+                tmp.write_all(b"Contents of depot file")
+                    .expect("Write failed");
+
+                tmp.path().to_path_buf()
+            }
+        }
+    };
+
+    (handle_path(path_source), handle_path(path_target))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parsing() {
+        let depot = UniversalPath::new("//stream/main/file.txt");
+        assert!(matches!(depot, UniversalPath::Depot(_)));
+        assert_eq!(depot.to_p4_string(), "//stream/main/file.txt");
+
+        let local = UniversalPath::new(r"C:\User\File.txt");
+        assert!(matches!(local, UniversalPath::Local(_)));
+        assert_eq!(local.to_p4_string(), "C:/User/File.txt");
+    }
+}
